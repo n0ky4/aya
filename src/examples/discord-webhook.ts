9864127192c7c6739cd80e4f1942da898dcfe75f
n0ky4 <n0ky4@proton.me>
2024-10-01 20:00:44 -0300

    [feat] add show load message

--- src/examples/discord-webhook.ts
+++ src/examples/discord-webhook.ts
@@ -6,6 +6,7 @@ const log = new Logger().use(
         url: 'https://discord.com/api/webhooks/1234567890/ABCDEFGHIJKLMNOPQRSTUVWXYZ',
         username: 'Rei',
         avatarUrl: 'https://i.imgur.com/L2vBNwc.jpeg',
+        showLoadMessage: true,
         levels: ['error'] // Only send messages with the level 'error' to Discord
     })
 )
